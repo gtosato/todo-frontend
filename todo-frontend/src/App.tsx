065c79292776b8aaf009f6558059274c661dde56
Greg Tosato <gregtosato@gmail.com>
2024-03-11 09:43:42 +1100

    toast update

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './pages/HomePage/HomePage';
 import CreatePage from './pages/CreatePage/CreatePage';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 function App() {
 
   return (
@@ -10,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/create" element={<CreatePage />} /> 
+
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   )
 }
--- todo-frontend/src/pages/HomePage/HomePage.tsx
+++ todo-frontend/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getAllToDoListPosts } from '../../services/to-do-list-services';
 import ToDoList from '../../containers/ToDoList/ToDoList';
+import { Flip, toast } from 'react-toastify';
 
 export interface ToDoProps {
   id: number;
@@ -13,15 +14,32 @@ const Homepage = () => {
 
   const [todoData, setTodoData] = useState<ToDoProps[]>([]);
 
+  const notify = (error: Error) => {
+    toast.error(`Error retrieving To Do List records: ${error.message}`, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Flip,
+    });    
+  }
+
   useEffect(() => {
     getAllToDoListPosts()
       .then((data) => setTodoData(data))
-      .catch((e) => console.warn(e.message));
+      .catch((e) => {
+        // console.warn(e.message)
+        notify(e)
+      });
   }, []);
 
   return (
     <div>
-        <ToDoList todoData={todoData} setTodoData={setTodoData} />          
+      <ToDoList todoData={todoData} setTodoData={setTodoData} />   
     </div>
   )
 }
